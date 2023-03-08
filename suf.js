18676823285490c11ea3ded546d7c49d8534ae1b
OneTooMany <santosismael.115@gmail.com>
2023-03-08 13:04:17 -0800

    Create an eventlistener when form is submitted changin border color if password inputs when not matching. preventDefault is used to prevent page reloading

--- suf.js
+++ suf.js
@@ -1,9 +1,16 @@
 const valUserPassword = document.querySelector('#user_password');
 const valConfirmPassword = document.querySelector('#confirm_password');
 const input = document.querySelectorAll('input');
+const form = document.querySelector('form')
 
-function validatePassword() {
-    if (valUserPassword.textContent != valConfirmPassword.textContent) {
-        input.style.border =  "2.3px solid green";
-    }
+ const submit = (e) => {
+    e.preventDefault();
+     if(valUserPassword.value !== valConfirmPassword.value) {
+        valUserPassword.style.border = "1.8px solid #c93e3e";
+        valConfirmPassword.style.border = "1.8px solid #c93e3e";    
+        return;
+    } 
+    console.log('submit');
 }
+
+ form.addEventListener('submit', submit) 
